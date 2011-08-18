c1b39eb0d8515a3b672ff5dee55fc9258da402aa
Charles Ouellet <couellet@spektrummedia.com>
2011-08-18 11:12:50 -0400

    Initial commit

---
+++ jquery.sum.js
@@ -0,0 +1,22 @@
+﻿(function($) {
+    $.fn.sum = function(callback) {
+        var sum = 0;
+        $(this).each(function() {
+            var val = $(this).val();
+
+            if (!val || isNaN(val)) {
+                val = $(this).text();
+            }
+
+            if (val != "" && !isNaN(val)) {
+                sum += parseFloat(val);
+            }
+        });
+
+        if (callback) {
+            callback(sum);
+        }
+
+        return sum;
+    }
+})(jQuery);
